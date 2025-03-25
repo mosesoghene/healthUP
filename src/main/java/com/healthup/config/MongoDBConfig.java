1eeb1adabc60cb04437145576c3ca8249c5da6d9
Moses Oghene <mosesogheneo@gmail.com>
2025-03-25 13:31:12 +0100

    Database connection - MongoDB

--- src/main/java/com/healthup/config/MongoDBConfig.java
+++ src/main/java/com/healthup/config/MongoDBConfig.java
@@ -1,4 +1,18 @@
 package com.healthup.config;
 
-public class MongoDBConfig {
+import com.mongodb.client.MongoClients;
+
+public class MongoDBConfig implements DatabaseConfig {
+    private static final String CONNECTION_STRING = "mongodb://localhost:27017";
+    private static final String DATABASE_NAME = "health_management";
+
+    @Override
+    public Object getConnection() throws Exception {
+        return MongoClients.create(CONNECTION_STRING);
+    }
+
+    @Override
+    public void closeConnection(Object connection) throws Exception {
+
+    }
 }
